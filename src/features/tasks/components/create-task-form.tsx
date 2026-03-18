ab5838fac48a8397a27a5f6986e3e312a0d8aee7
rahulsirohi077 <rahulsirohi077@gmail.com>
2026-03-18 13:52:08 +0530

    feat: enhance CreateTaskForm with due date and assignee selection, and add DatePicker component

--- src/features/tasks/components/create-task-form.tsx
+++ src/features/tasks/components/create-task-form.tsx
@@ -1,9 +1,16 @@
-"use client"
+"use client";
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { cn } from "@/lib/utils";
@@ -16,93 +23,225 @@ import { useForm } from "react-hook-form";
 import z from "zod";
 import { useCreateTask } from "../api/use-create-task";
 import { createTaskSchema } from "../schema";
+import { DatePicker } from "@/components/date-picker";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import MemberAvatar from "@/features/members/components/members-avatar";
+import { TaskStatus } from "../types";
+import ProjectAvatar from "@/features/projects/components/project-avatar";
 
 interface CreateTaskFormProps {
-    onCancel?: () => void;
-    projectOptions: {id:string,name:string, imageUrl:string | undefined}[],
-    memberOptions: {id:string,name:string}[]
+  onCancel?: () => void;
+  projectOptions: { id: string; name: string; imageUrl: string | undefined }[];
+  memberOptions: { id: string; name: string }[];
 }
 
-export const CreateTaskForm = ({ onCancel, projectOptions, memberOptions }: CreateTaskFormProps) => {
-    const workspaceId = useWorkspaceId();
-    const router = useRouter();
-    const { mutate, isPending } = useCreateTask();
+export const CreateTaskForm = ({
+  onCancel,
+  projectOptions,
+  memberOptions,
+}: CreateTaskFormProps) => {
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
+  const { mutate, isPending } = useCreateTask();
 
-    const form = useForm<
-        z.input<typeof createTaskSchema>,
-        unknown,
-        z.output<typeof createTaskSchema>
-    >({
-        resolver: zodResolver(createTaskSchema),
-        defaultValues: {
-            workspaceId: workspaceId
-        }
-    })
+  const form = useForm<z.input<typeof createTaskSchema>, unknown, z.output<typeof createTaskSchema>>({
+    resolver: zodResolver(createTaskSchema),
+    defaultValues: {
+      workspaceId,
+      name: "",
+      assigneeId: "",
+      projectId: "",
+      status: TaskStatus.BACKLOG, 
+      dueDate: undefined,
+      description: "",
+    },
+  });
 
-    const onSubmit = (values: z.output<typeof createTaskSchema>) => {
-        mutate({ json: {...values, workspaceId} }, {
-            onSuccess: ({ data }) => {
-                form.reset();
-                // TODO: redirect to new task 
-            }
-        })
-    }
+  const onSubmit = (values: z.output<typeof createTaskSchema>) => {
+    mutate(
+      { json: { ...values, workspaceId } },
+      {
+        onSuccess: ({ data }) => {
+          form.reset();
+          onCancel?.();
+        },
+      },
+    );
+  };
 
-    return (
-        <Card className="w-full h-full shadow-none border-none">
-            <CardHeader className="flex">
-                <CardTitle className="text-xl font-bold">
-                    Create a new Task
-                </CardTitle>
-            </CardHeader>
-            <div className="px-7">
-                <DottedSeparator />
+  return (
+    <Card className="w-full h-full shadow-none border-none">
+      <CardHeader className="flex">
+        <CardTitle className="text-xl font-bold">Create a new Task</CardTitle>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Task name</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Enter Task name"
+                        autoComplete="off"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="dueDate"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Due Date</FormLabel>
+                    <FormControl>
+                      <DatePicker
+                        value={
+                          field.value instanceof Date ? field.value : undefined
+                        }
+                        onChange={field.onChange}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="assigneeId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Assignee</FormLabel>
+                    <Select
+                      defaultValue={field.value}
+                      onValueChange={field.onChange}
+                    >
+                      <FormControl>
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="Select assignee" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <FormMessage />
+                      <SelectContent>
+                        {memberOptions.map((member) => (
+                          <SelectItem key={member.id} value={member.id}>
+                            <div className="flex items-center gap-x-2">
+                              <MemberAvatar
+                                className="size-6"
+                                name={member.name}
+                              />
+                              {member.name}
+                            </div>
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="status"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Status</FormLabel>
+                    <Select
+                      defaultValue={field.value}
+                      onValueChange={field.onChange}
+                    >
+                      <FormControl>
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="Select Status" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <FormMessage />
+                      <SelectContent>
+                        <SelectItem value={TaskStatus.BACKLOG}>
+                          BACKLOG
+                        </SelectItem>
+                        <SelectItem value={TaskStatus.IN_PROGRESS}>
+                          IN_PROGRESS
+                        </SelectItem>
+                        <SelectItem value={TaskStatus.IN_REVIEW}>
+                          IN_REVIEW
+                        </SelectItem>
+                        <SelectItem value={TaskStatus.TODO}>TODO</SelectItem>
+                        <SelectItem value={TaskStatus.DONE}>DONE</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="projectId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Project</FormLabel>
+                    <Select
+                      defaultValue={field.value}
+                      onValueChange={field.onChange}
+                    >
+                      <FormControl>
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="Select Project" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <FormMessage />
+                      <SelectContent>
+                        {projectOptions.map((project) => (
+                          <SelectItem key={project.id} value={project.id}>
+                            <div className="flex items-center gap-x-2">
+                              <ProjectAvatar
+                                className="size-6"
+                                name={project.name}
+                                image={project.imageUrl}
+                              />
+                              {project.name}
+                            </div>
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
             </div>
-            <CardContent>
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-                        <div className="space-y-4">
-                            <FormField
-                                control={form.control}
-                                name="name"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Task name</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                {...field}
-                                                placeholder="Enter Task name"
-                                                autoComplete="off"
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                        </div>
-                        <DottedSeparator className="px-7" />
-                        <div className="flex items-center justify-between">
-                            <Button
-                                type="button"
-                                size={'lg'}
-                                variant={'secondary'}
-                                onClick={onCancel}
-                                className={cn(!onCancel && "invisible")}
-                                disabled={isPending}
-                            >
-                                Cancel
-                            </Button>
-                            <Button
-                                type="submit"
-                                size={'lg'}
-                                disabled={isPending}
-                            >
-                                Create Task
-                            </Button>
-                        </div>
-                    </form>
-                </Form>
-            </CardContent>
-        </Card>
-    )
-}
+            <DottedSeparator className="px-7" />
+            <div className="flex items-center justify-between">
+              <Button
+                type="button"
+                size={"lg"}
+                variant={"secondary"}
+                onClick={onCancel}
+                className={cn(!onCancel && "invisible")}
+                disabled={isPending}
+              >
+                Cancel
+              </Button>
+              <Button type="submit" size={"lg"} disabled={isPending}>
+                Create Task
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+};
